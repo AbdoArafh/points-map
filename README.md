4988caf09c49cfdd2ad04f2fba2b481d7ee21149
AbdoArafh54 <abdoarafh54@gmail.com>
2021-08-12 20:18:38 +0200

    finished making it a class and refactored the code

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Points Map
+
+this is a points map made to display json data about shipping requests
+
+### Quick Start
+
+```html
+<div id="mapId"></div>
+```
+
+```js
+const map = new Map(mapId, jsonDataUrl, isDarkMode);
+map.showMap();
+```
+
+### Details
+
+Argument Name | type | default | details
+mapId | string | ** | you set it as you like 😁
+jsonDataUrl | string | ** | the Url to fetch/request the data from
+isDarkMode | boolean | true | set it to true to enable dark mode
+
+### Methods
--- main.js
+++ main.js
@@ -1,83 +1,102 @@
-let map;
+class Map {
+  constructor(mapid, jsonLink, isDarkMode = true) {
+    this.map = L.map(mapid, { center: [0, 0], zoom: 1 });
+    this.jsonLink = jsonLink;
+    this.data = [];
+    this.circleMarkers = [];
+    this.isDarkMode = isDarkMode;
+    this.updateDataArray = this.updateDataArray.bind(this);
+    this.fitBounds = this.fitBounds.bind(this);
+    this.showData = this.showData.bind(this);
+    this.setMinAndMaxRadius = this.setMinAndMaxRadius.bind(this);
+    this.setMode = this.setMode.bind(this);
+    this.showMap = this.showMap.bind(this);
+  }
 
-const switchMode = () => {
-  if (darkMode)
-    tileLayer.setUrl(
-      "https://api.mapbox.com/styles/v1/abdoarafh/cks4ojlbg4ncb18o4jy282kka/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}"
-    );
-  else
-    tileLayer.setUrl(
-      "https://api.mapbox.com/styles/v1/abdoarafh/cks93mkwf293m17o3p5309fkq/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}"
-    );
-};
-
-const showMap = (mapid = "map", initialCoords = [0, 0], initialZoom = 1) => {
-  map = L.map(mapid, { center: initialCoords, zoom: initialZoom });
-
-  tileLayer = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/abdoarafh/cks4ojlbg4ncb18o4jy282kka/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}",
-    {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      minZoom: 1,
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken:
-        "pk.eyJ1IjoiYWJkb2FyYWZoIiwiYSI6ImNrczRtcHFiNTFzcnMycHBqbHljenYyZWgifQ.HxnT-zA7MXy4ph9_TGcYZQ",
-    }
-  );
-
-  tileLayer.addTo(map);
-
-  getDataArray()
-    .then(data => fitBounds(data))
-    .then(data => showData(data));
-};
-
-const showData = data => {
-  const primaryFontColor = "#41b883";
-  const minRadius = 5;
-  const maxRadius = 20 - minRadius;
-  const requests = data.map(element => element.requests);
-  const maxRequest = Math.max(...requests);
-
-  data.forEach(el => {
-    const circle = L.circleMarker([el.lat, el.long], {
-      color: primaryFontColor,
-      fillColor: "#ff9900",
-      fillOpacity: 0.8,
-      radius: (el.requests / maxRequest) * maxRadius + minRadius,
-      weight: 0,
-    }).addTo(map);
-
-    circle.bindTooltip(
-      `<div style="text-align: center"><h4 style="padding: 0; margin: 0;">Requests</h4><p style="color: ${primaryFontColor};padding: 0; margin: 0;">${el.requests}</p></div>`,
+  showMap() {
+    this.tileLayer = L.tileLayer(
+      "https://api.mapbox.com/styles/v1/abdoarafh/cks4ojlbg4ncb18o4jy282kka/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}",
       {
-        direction: "top",
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        minZoom: 1,
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1IjoiYWJkb2FyYWZoIiwiYSI6ImNrczRtcHFiNTFzcnMycHBqbHljenYyZWgifQ.HxnT-zA7MXy4ph9_TGcYZQ",
       }
     );
-  });
-};
+    this.tileLayer.addTo(this.map);
+    this.setMode(this.isDarkMode ? "dark" : "light");
+    this.updateDataArray();
+  }
 
-const fitBounds = data => {
-  const areasLat = data.map(element => element.lat);
-  const areasLng = data.map(element => element.long);
-  const topLeftCorner = L.latLng(Math.min(...areasLat), Math.min(...areasLng));
-  const bottomRightCorner = L.latLng(
-    Math.max(...areasLat),
-    Math.max(...areasLng)
-  );
-  map.fitBounds(L.latLngBounds(topLeftCorner, bottomRightCorner));
-  return data;
-};
+  setMinAndMaxRadius(minr, maxr) {
+    this.minRadius = minr;
+    this.maxRadius = maxr;
+  }
 
-const getDataArray = async (link = "data.json") => {
-  const response = await fetch(link);
-  const arr = await response.json();
-  return arr;
-};
+  showData() {
+    this.primaryFontColor = "#41b883";
+    this.minRadius = 5;
+    this.maxRadius = 15;
+    const requests = this.data.map(element => element.requests);
+    const maxRequest = Math.max(...requests);
 
-const avg = arr => arr.reduce((acc, curr) => acc + curr) / arr.length;
+    this.circleMarkers = this.data.map(el => {
+      const circle = L.circleMarker([el.lat, el.long], {
+        fillColor: "#ff9900",
+        fillOpacity: 0.8,
+        radius: (el.requests / maxRequest) * this.maxRadius + this.minRadius,
+        weight: 0,
+      }).addTo(this.map);
 
-// showMap("map");
+      circle.bindTooltip(
+        `<div style="text-align: center"><h4 style="padding: 0; margin: 0;">Requests</h4><p style="color: ${this.primaryFontColor};padding: 0; margin: 0;">${el.requests}</p></div>`,
+        {
+          direction: "top",
+        }
+      );
+      return circle;
+    });
+  }
+
+  updateDataArray() {
+    fetch(this.jsonLink)
+      .then(response => response.json())
+      .then(data => (this.data = data))
+      .then(() => this.fitBounds())
+      .then(() => this.showData());
+  }
+
+  fitBounds() {
+    const areasLat = this.data.map(element => element.lat);
+    const areasLng = this.data.map(element => element.long);
+    const topLeftCorner = L.latLng(
+      Math.min(...areasLat),
+      Math.min(...areasLng)
+    );
+    const bottomRightCorner = L.latLng(
+      Math.max(...areasLat),
+      Math.max(...areasLng)
+    );
+    this.map.fitBounds(L.latLngBounds(topLeftCorner, bottomRightCorner));
+  }
+
+  avg(arr) {
+    return arr.reduce((acc, curr) => acc + curr) / arr.length;
+  }
+
+  setMode(mode) {
+    this.isDarkMode = mode === "dark";
+    if (this.isDarkMode)
+      this.tileLayer.setUrl(
+        "https://api.mapbox.com/styles/v1/abdoarafh/cks4ojlbg4ncb18o4jy282kka/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}"
+      );
+    else
+      this.tileLayer.setUrl(
+        "https://api.mapbox.com/styles/v1/abdoarafh/cks93mkwf293m17o3p5309fkq/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}"
+      );
+  }
+}
