0d3c57ecf5fd7e2e99470bff680f4136446dd381
AbdoArafh54 <abdoarafh54@gmail.com>
2021-08-12 18:24:28 +0200

    added dark and light mode

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 let map;
 
+const switchMode = () => {
+  if (darkMode)
+    tileLayer.setUrl(
+      "https://api.mapbox.com/styles/v1/abdoarafh/cks4ojlbg4ncb18o4jy282kka/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}"
+    );
+  else
+    tileLayer.setUrl(
+      "https://api.mapbox.com/styles/v1/abdoarafh/cks93mkwf293m17o3p5309fkq/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}"
+    );
+};
+
 const showMap = (mapid = "map", initialCoords = [0, 0], initialZoom = 1) => {
   map = L.map(mapid, { center: initialCoords, zoom: initialZoom });
 
-  L.tileLayer(
+  tileLayer = L.tileLayer(
     "https://api.mapbox.com/styles/v1/abdoarafh/cks4ojlbg4ncb18o4jy282kka/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}",
     {
       attribution:
@@ -15,7 +26,9 @@ const showMap = (mapid = "map", initialCoords = [0, 0], initialZoom = 1) => {
       accessToken:
         "pk.eyJ1IjoiYWJkb2FyYWZoIiwiYSI6ImNrczRtcHFiNTFzcnMycHBqbHljenYyZWgifQ.HxnT-zA7MXy4ph9_TGcYZQ",
     }
-  ).addTo(map);
+  );
+
+  tileLayer.addTo(map);
 
   getDataArray()
     .then(data => fitBounds(data))
